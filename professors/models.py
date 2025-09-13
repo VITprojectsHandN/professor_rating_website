5ab14fe6fedc5fc8256e3c8bc5c606129599b0d8
nIKHIL130805 <nikhil130805@gmail.com>
2025-09-13 19:55:37 +0530

    Final

--- professors/models.py
+++ professors/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 class Professor(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)  # duplicates allowed
 
     def __str__(self):
