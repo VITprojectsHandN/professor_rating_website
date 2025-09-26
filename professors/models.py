ad39375ba1d8bf2ca014149ed65b284da41893c8
nIKHIL130805 <nikhil130805@gmail.com>
2025-09-26 15:45:44 +0530

    case

--- professors/models.py
+++ professors/models.py
@@ -5,13 +5,6 @@ class Professor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # match Supabase UUID
     name = models.CharField(max_length=255, unique=True)
 
-    class Meta:
-        db_table = "professors"  # must match Supabase table name
-
-    def save(self, *args, **kwargs):
-        self.name = self.name.title()  # Ensure name is always Title Case
-        super().save(*args, **kwargs)
-
 
 class Rating(models.Model):
     professor = models.ForeignKey(
